f08b258fca42355a10ab34b7e7a54a2bf1b3c91a
p2vvel <p2vvel@gmail.com>
2021-09-18 00:33:55 +0200

    Code working, more tests to write and code cleaning to do

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
 
-from wiki.main import ArticleException, WikiArticle
+from wiki.__main__ import ArticleException, WikiArticle
 
-from .links import *
+from links import *
 
 import requests
 
@@ -19,7 +19,7 @@ class AllTests(TestCase):
         '''Checks if names are correctly scrapped from wikipedia links by checking if there is an article with such name'''
         for k in get_links():
             try:
-                article = WikiArticle(k)
+                article = WikiArticle(k, 500)
                 #i had to change user agent, because wiki servers were blocking me (returning 403 - "Forbidden")
                 response = requests.get("http://en.wikipedia.org/wiki/%s" %
                                         article.title)
@@ -33,12 +33,12 @@ class AllTests(TestCase):
         '''checking if article without thumbnail will raise an exception'''
         articles = [
             'https://en.wikipedia.org/wiki/1952_Formula_One_season',
-            "https://pl.wikipedia.org/wiki/Shrek",
+            "https://pl.wikipedia.org/wiki/The_Eminem_Show",
             "https://pl.wikipedia.org/wiki/My_Beautiful_Dark_Twisted_Fantasy"
         ]
 
         for k in articles:
-            self.assertRaises(ArticleException, WikiArticle, k)
+            self.assertRaises(ArticleException, WikiArticle, k, 500)
 
     def test_api_scrapping(self):
         '''tests if api urls are correct'''
@@ -66,7 +66,7 @@ class AllTests(TestCase):
                                  msg="ERROR: %s " % article)
             except ArticleException as e:
                 pass  #no need to check articles without thumbnail
-    
+
     def test_original_thumbnails_urls(self):
         '''tests if urls for fullsize thumbnails are scrapped correctly'''
         for k in get_links(2):
@@ -78,8 +78,77 @@ class AllTests(TestCase):
                         "User-Agent":
                         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0"
                     })
-                self.assertEqual(response.status_code,
-                                 200,
-                                 msg="ERROR: %s, %s " % (article, article.get_original_thumbnail_url()))
+                self.assertEqual(
+                    response.status_code,
+                    200,
+                    msg="ERROR: %s, %s " %
+                    (article, article.get_original_thumbnail_url()))
             except ArticleException as e:
                 pass  #no need to check articles without thumbnail
+
+    def test_saving_thumbnails_no_filename(self):
+        '''tests if saving thumbnails works properly'''
+        articles = [
+            'https://en.wikipedia.org/wiki/1951_Formula_One_season',
+            "https://pl.wikipedia.org/wiki/Shrek",
+            "https://pl.wikipedia.org/wiki/Kanye_West"
+        ]
+
+        for url in articles:
+            article = WikiArticle(url, 500)
+            article.save_thumbnail()
+            file_data = open(WikiArticle.title_from_link(article.get_thumbnail_url()), "rb").read()  #wczytuje plik
+            img_data = requests.get(article.get_thumbnail_url(), headers=article.header).content
+            self.assertEqual(file_data, img_data)
+
+    def test_saving_thumbnails_no_filename(self):
+        '''tests if saving thumbnails works properly'''
+        articles = [
+            'https://en.wikipedia.org/wiki/1951_Formula_One_season',
+            "https://pl.wikipedia.org/wiki/Shrek",
+            "https://pl.wikipedia.org/wiki/Kanye_West"
+        ]
+
+        names = ["formula", "shrek.jpg", "ye.ezy"]
+
+        for url, name in zip(articles, names):
+            article = WikiArticle(url, 500)
+            article.save_thumbnail(name)
+            temp = WikiArticle.title_from_link(article.get_thumbnail_url())
+            file_data = open(temp, "rb").read()  #wczytuje plik
+            img_data = requests.get(article.get_thumbnail_url(), headers=article.header).content
+            self.assertEqual(file_data, img_data)
+
+
+    def test_links_to_title(self):
+        '''tests if turning links to images filenames works properly'''
+        links = [
+            "https://upload.wikimedia.org/wikipedia/commons/4/44/Sebastian_Vettel_2015_Malaysia_podium_1.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Alfa-Romeo-159-%281951%29.jpg/500px-Alfa-Romeo-159-%281951%29.jpg",
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Eminem_-_Concert_for_Valor_in_Washington%2C_D.C._Nov._11%2C_2014_%282%29_%28Cropped%29.jpg/416px-Eminem_-_Concert_for_Valor_in_Washington%2C_D.C._Nov._11%2C_2014_%282%29_%28Cropped%29.jpg',
+        ]
+
+        filenames = [
+            "Sebastian_Vettel_2015_Malaysia_podium_1.jpg",
+            "500px-Alfa-Romeo-159-%281951%29.jpg",
+            "416px-Eminem_-_Concert_for_Valor_in_Washington%2C_D.C._Nov._11%2C_2014_%282%29_%28Cropped%29.jpg"
+        ]
+
+        for l, f in zip(links, filenames):
+            self.assertEqual(WikiArticle.title_from_link(l), f)
+
+    def test_localisation(self):
+        '''tests if country subdomain is correctly fetched from wiki url'''
+        articles = [
+            'https://en.wikipedia.org/wiki/1951_Formula_One_season',
+            "https://pl.wikipedia.org/wiki/Shrek",
+            "https://pl.wikipedia.org/wiki/Kanye_West"
+        ]
+        subdomains = ["en", "pl", "szl"]
+
+        for l, s in zip(links, subdomains):
+            try:
+                article = WikiArticle(l, 500)
+                self.assertEqual(s, article.localisation)
+            except:
+                self.assertEqual(s, article.localisation)
--- wiki/__main__.py
+++ wiki/__main__.py
@@ -2,16 +2,32 @@ import requests
 from requests.models import Response
 
 
-
 class ArticleException(Exception):
     pass
 
 
 class WikiArticle:
-    def __init__(self, url, thumbsize=None):
+    def title_from_link(image_url):
+        '''Converts wikipedia image link to title for example: 
+            "https://upload.wikimedia.org/wikipedia/commons/4/44/Sebastian_Vettel_2015_Malaysia_podium_1.jpg"
+            -> 
+            "Sebastian_Vettel_2015_Malaysia_podium_1.jpg"
+        '''
+        title = image_url[image_url.rindex("/") + 1:]
+        return title
+
+
+    def __init__(self, url, thumbsize, user_agent=""):
+        self.thumbsize = thumbsize
+        self.url = url
+        self.localisation = url[url.index("://") + 3 : url.index(".")]  #'pl', 'en', 'com' etc.
+        self.header = {
+            "User-Agent": user_agent
+        } if user_agent else {
+            "User-Agent":
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
+        }
         try:
-            self.thumbsize = thumbsize or 500
-            self.url = url
             self.title = self.get_title()
             self.api_url = self.get_api_url()
             self.api_json = self.get_api_json()
@@ -34,45 +50,130 @@ class WikiArticle:
             return title
 
     def get_api_url(self):
-        return "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&titles=%s&pithumbsize=%s" % (
-            self.title, self.thumbsize)
+        return "https://%s.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&titles=%s&pithumbsize=%s" % (
+            self.localisation, self.title, self.thumbsize)
 
     def get_api_json(self):
-        response = requests.get(self.api_url)
+        response = requests.get(self.api_url, headers=self.header)
+        if response.status_code != 200:
+            raise ArticleException("Unable to fetch API data!")
         return response.json()
 
     def get_thumbnail_data(self):
         try:
             page_id = [k for k in self.api_json["query"]["pages"].keys()][0]
             thumbnail = self.api_json["query"]["pages"][page_id]["thumbnail"]
-            # print("JEST?", "thumbnail" in self.api_json["query"]["pages"][page_id])
             return thumbnail
         except KeyError:
-            raise ArticleException("Article without thumbnail!")
+            raise ArticleException(
+                "Article without thumbnail or API didnt returned any thumbnail data!"
+            )
 
     def get_thumbnail_url(self):
         return self.thumbnail_data["source"]
 
     def get_original_thumbnail_url(self):
         url = self.thumbnail_data["source"]
+        #if theres no /thumb/ part in thumbnail url, you were given fullsize thumbnail url, no need to change anything
         if "/thumb/" in url:
             url = url.replace("/thumb/", "/")  #removes "thumb/" part from url
             url = url[:url.rindex("/")]
-            return url
+        return url
+
+    def save_thumbnail(self, filename = ""):
+        #saves thumbnail as file, locally
+        image_url = self.get_thumbnail_url()
+        if filename == "":
+            filename = WikiArticle.title_from_link(image_url)
         else:
-            #if theres no /thumb/ part in thumbnail url, you were given fullsize thumbnail url, no need to change anything
-            return url
+            extension = image_url[image_url.rindex("."):]   #file extension is at the end of the url
+            filename = filename + extension #adding extension even if user has added one - for safety
+
+        response = requests.get(image_url, headers=self.header)
+        with open(filename, "wb") as file:
+            file.write(response.content)
+
+    def save_original_thumbnail(self, filename = ""):
+        #saves fullsize thumbnail as file, locally
+        image_url = self.get_original_thumbnail_url()
+        if filename == "":
+            filename = WikiArticle.title_from_link(image_url)
+        else:
+            extension = image_url[image_url.rindex("."):]   #file extension is at the end of the url
+            filename = filename + extension #adding extension even if user has added one - for safety
+
+        response = requests.get(image_url, headers=self.header)
+        with open(filename, "wb") as file:
+            file.write(response.content)
 
 
-if __name__ == "__main__":
+def show_article(url, size, type, **kwargs):
+    article = WikiArticle(url, size)
+    if type == "thumbnail":
+        print(article.get_thumbnail_url())
+    elif type == "original":
+        print(article.get_original_thumbnail_url())
+
+
+def save_article(url, size, type, filename, **kwargs):
+    article = WikiArticle(url, size)
+    if type=="thumbnail":
+        article.save_thumbnail(filename)
+    else:
+        article.save_original_thumbnail(filename)
+    
+
+
+def main():
     import sys
     args = sys.argv
 
     import argparse
 
     parser = argparse.ArgumentParser(prog="Wiki Thumbnails Downloader")
-    parser.add_argument("show", nargs="?", help="shows link to article thumbnail")
-    parser.add_argument("save", help="shows link to article thumbnail")
-    parser.print_help()
-    # article = WikiArticle(args[1])
-    # print(article.get_thumbnail_url())
+    subparsers = parser.add_subparsers()
+
+    show_subparser = subparsers.add_parser("show", help="show thumbnail url")
+    show_subparser.add_argument("--url",
+                                type=str,
+                                help="wiki article url",
+                                required=True)
+    show_subparser.add_argument(
+        "--size",
+        type=int,
+        help="max thumbnail size(might be smaller) [default=500px]",
+        default=500)
+    show_subparser.add_argument(
+        "--type",
+        choices=["original", "thumbnail"],
+        default="thumbnail",
+        help="choose between thumbnail or original image")
+    show_subparser.set_defaults(func=show_article)
+
+    save_subparser = subparsers.add_parser("save", help="save thumbnail")
+    save_subparser.add_argument("--url",
+                                type=str,
+                                help="wiki article url",
+                                required=True)
+    save_subparser.add_argument(
+        "--size",
+        type=int,
+        help="max thumbnail size(might be smaller) [default=500px]",
+        default=500)
+    save_subparser.add_argument(
+        "--type",
+        choices=["original", "thumbnail"],
+        default="thumbnail",
+        help="choose between thumbnail or original image")
+    save_subparser.add_argument("--filename",
+                                type=str,
+                                default="",
+                                help="image filename")
+    save_subparser.set_defaults(func=save_article)
+
+    args = parser.parse_args()
+    args.func(**args.__dict__)
+
+
+if __name__ == "__main__":
+   main()
